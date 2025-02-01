6158316a1e82255ce9266f99b6fbed7d8577a3bf
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2025-02-01 01:52:05 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,44 +10,47 @@ def main():
 
         userInput = input()
         command, parameter = parseInput(userInput)
-        
-        if command=="exit":
-            if parameter:
-                exit(int(parameter))
-            else:
-                exit()
-        elif command=="echo":
-            print(f"{parameter}")
-        elif command=="type":
-            if parameter:
-                cmd = parameter.split(" ")[0]
-                paths = PATH.split(":")
-                cmd_path = fileFromPath(cmd,PATH)
-                if cmd in ['echo', 'type', 'exit', 'pwd']:
-                    print(f"{cmd} is a shell builtin")
-                elif cmd_path is not None:
-                    print(f"{cmd} is {cmd_path}")
+        match command:
+            case "exit":
+                if parameter:
+                    exit(int(parameter))
                 else:
-                    print(f"{cmd} not found")
-        elif command=="pwd":
-            if not parameter:
-                print(os.getcwd())
-            else:
-                print("pwd: too many arguments")
-        elif command=="cd":
-            if not parameter or parameter=="~":
-                os.chdir(os.getenv("HOME"))
-            else:
-                if(os.path.isdir(f"{parameter}")):
-                    os.chdir(parameter)
+                    exit()
+            case "echo":
+                print(f"{parameter}")
+                continue
+            case "type":
+                if parameter:
+                    cmd = parameter.split(" ")[0]
+                    cmd_path = fileFromPath(cmd,PATH)
+                    if cmd in ['echo', 'type', 'exit', 'pwd']:
+                        print(f"{cmd} is a shell builtin")
+                    elif cmd_path is not None:
+                        print(f"{cmd} is {cmd_path}")
+                    else:
+                        print(f"{cmd} not found")
+                    continue
+            case "pwd":
+                if not parameter:
+                    print(os.getcwd())
                 else:
-                    print(f"cd: {parameter}: No such file or directory")
-        else:
-            cmd_file = fileFromPath(command,PATH)
-            if cmd_file is not None:
-                os.system(userInput)
-            else:
-                print(f"{command}: command not found")
+                    print("pwd: too many arguments")
+                continue
+            case "cd":
+                if not parameter or parameter=="~":
+                    os.chdir(os.getenv("HOME"))
+                else:
+                    if(os.path.isdir(f"{parameter}")):
+                        os.chdir(parameter)
+                    else:
+                        print(f"cd: {parameter}: No such file or directory")
+                continue
+            case _:
+                cmd_file = fileFromPath(command,PATH)
+                if cmd_file is not None:
+                    os.system(userInput)
+                else:
+                    print(f"{command}: command not found")
 
 def parseInput(command):
     parameter=""
@@ -56,21 +59,6 @@ def parseInput(command):
         parameter = parseQuotes(parameter)
         command = command[0:command.find(" ")]
     return command,parameter
-
-# def parseSingleQuotes(parameter):
-#     inSQ = False
-#     retStr=""
-#     for i in range(len(parameter)):
-#         if parameter[i:i+1]=="\'":
-#             inSQ = not inSQ
-#             continue
-#         if inSQ:
-#             retStr = retStr + parameter[i:i+1]
-#         else:
-#             if parameter[i:i+1] == " " and retStr[len(retStr)-1:] == " ":
-#                 continue
-#             retStr = retStr + parameter[i:i+1]
-#     return retStr    
 
 def parseQuotes(parameter):
     inDQ = False
