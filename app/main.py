4f8b6e850d42b196b68b22ed2961a79125c5fc65
Maaz Chowdhry <maazchowdhry2001@gmail.com>
2025-02-01 13:16:35 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import sys
 import os
 from typing import Tuple
 
+class MyCustomError(Exception):
+    pass
+
 def main():
     PATH:str = os.environ.get("PATH")
     while True:
@@ -9,12 +12,22 @@ def main():
         sys.stdout.flush()
 
         toFile=None
+        toErrorFile=None
         userInput:str = input()
         command, parameter = parseInput(userInput)
+
         if "1>" in parameter:
             parts = parameter.split("1>", 1)
             parameter = parts[0].strip()
             toFile = parts[1].strip()
+
+        elif "2>" in parameter:
+            parts = parameter.split("2>", 1)
+            parameter = parts[0].strip()
+            toErrorFile = parts[1].strip()
+            os.makedirs(os.path.dirname(toErrorFile), exist_ok=True)
+            with open(toErrorFile, "a") as file:
+                print("", end="", file=file)
 
         elif ">" in parameter:
             parts = parameter.split(">", 1)
@@ -23,56 +36,70 @@ def main():
 
         output=""
         # print(f"command: {command} parameter: {parameter}, toFile: {toFile}, output: {output}")
-        match command:
-            case "exit":
-                if parameter:
-                    exit(int(parameter))
-                else:
-                    exit()
-
-            case "echo":
-                output=f"{parameter}\n"
-
-            case "type":
-                if parameter:
-                    cmd = parameter.split(" ")[0]
-                    cmd_path = fileFromPath(cmd,PATH)
-                    if cmd in ['echo', 'type', 'exit', 'pwd']:
-                        output = f"{cmd} is a shell builtin\n"
-                    elif cmd_path is not None:
-                        output = f"{cmd} is {cmd_path}\n"
+        try:
+            match command:
+                case "exit":
+                    if parameter:
+                        exit(int(parameter))
                     else:
-                        output = f"{cmd} not found\n"
+                        exit()
 
-            case "pwd":
-                if not parameter:
-                    output = f"{os.getcwd()}\n"
-                else:
-                    output = "pwd: too many arguments\n"
+                case "echo":
+                    output=f"{parameter}\n"
 
-            case "cd":
-                if not parameter or parameter=="~":
-                    os.chdir(os.getenv("HOME"))
-                else:
-                    if(os.path.isdir(f"{parameter}")):
-                        os.chdir(parameter)
+                case "type":
+                    if parameter:
+                        cmd = parameter.split(" ")[0]
+                        cmd_path = fileFromPath(cmd,PATH)
+                        if cmd in ['echo', 'type', 'exit', 'pwd']:
+                            output = f"{cmd} is a shell builtin\n"
+                        elif cmd_path is not None:
+                            output = f"{cmd} is {cmd_path}\n"
+                        else:
+                            # output = f"{cmd} not found\n"
+                            raise MyCustomError(f"{cmd} not found\n")
+
+                case "pwd":
+                    if not parameter:
+                        output = f"{os.getcwd()}\n"
                     else:
-                        output = f"cd: {parameter}: No such file or directory\n"
+                        # output = "pwd: too many arguments\n"
+                        raise MyCustomError(f"pwd: too many arguments\n")
 
-            case _:
-                cmd_file = fileFromPath(command,PATH)
-                if cmd_file is not None:
-                    os.system(userInput)
-                else:
-                    output = f"{command}: command not found\n"
-        
-        if not toFile:
-            if output:
-                print(output, end="")
-        else:
+                case "cd":
+                    if not parameter or parameter=="~":
+                        os.chdir(os.getenv("HOME"))
+                    else:
+                        if(os.path.isdir(f"{parameter}")):
+                            os.chdir(parameter)
+                        else:
+                            # output = f"cd: {parameter}: No such file or directory\n"
+                            raise MyCustomError(f"cd: {parameter}: No such file or directory\n")
+
+                case _:
+                    cmd_file = fileFromPath(command,PATH)
+                    if cmd_file is not None:
+                        os.system(userInput)
+                    else:
+                        # output = f"{command}: command not found\n"
+                        raise MyCustomError(f"{command}: command not found\n")
+                    
+        except MyCustomError as e:
+            if toErrorFile:
+                os.makedirs(os.path.dirname(toErrorFile), exist_ok=True)
+                with open(toErrorFile, "a") as file:
+                    print(str(e), end="", file=file)
+            else:
+                print(str(e), end="")
+            continue
+
+        if toFile:
             os.makedirs(os.path.dirname(toFile), exist_ok=True)
             with open(toFile, "a") as file:
                 print(output, end="", file=file)
+        else:
+            if output:
+                print(output, end="")
 
 def parseInput(userInput:str) -> Tuple[str, str]: 
     parameter = ""
